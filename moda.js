caab8fb47321e1c116641cfc8f3f90906260a424
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2021-11-24 23:41:43 -0600

    Captura en input con tecla enter y otros bugs solucionados en calculo de moda

--- moda.js
+++ moda.js
@@ -1,23 +1,26 @@
+const listaUL = document.getElementById("capturaListaUL");
+const inputCapture = document.getElementById("dateArray");
+const buttonCapture = document.getElementById("buttonCapture");
+const buttonResult = document.getElementById("buttonResult");
+const buttonRestart = document.querySelector("#restart");
+const resultP = document.getElementById("ResultP");
 let resultModa;
+let listCaptureHTML = [];
 
 //Creación de función
 let listaCount = {};
-function moda (lista) {
-    //.map reccore cada elemento del array
+function moda (lista) {   
     lista.map(
-        function (elemento) {
-            //ahora asignamos los valores al objeto creado y vamos contabilizando.
+        function (elemento) {            
             if (listaCount[elemento]) {
                 listaCount[elemento] += 1;            
             } else {
                 listaCount[elemento] = 1;            
             }        
         }
-    );
-    //regresamos el objeto a otro array de array = [[],[],[]] con Object.entries
+    );    
     const listaArray = Object.entries(listaCount).sort(
-        function (elementoA, elementoB) {
-            //con la funcion de este metodo sort ordenamos los elementos
+        function (elementoA, elementoB) {      
             return elementoA[1] - elementoB[1];
         }
     );
@@ -28,59 +31,62 @@ function moda (lista) {
     return resultModa;
 }
 
-let listCaptureHTML = [];
-
 function crearArray (valor) {
     return listCaptureHTML.push(valor);
 }
 
-const listaUL = document.getElementById("capturaListaUL");
-const inputCapture = document.getElementById("dateArray");
-const buttonCapture = document.getElementById("buttonCapture");
-const buttonResult = document.getElementById("buttonResult");
-
-
-//HTML. capturar dato por dato
-const capturarDatoArray = function() {
-    let numberCapture = inputCapture.value;
-    crearArray(numberCapture);        
-    let etiquetaLI = document.createElement("li");
-    listaUL.appendChild(etiquetaLI);
-    etiquetaLI.innerText =`${numberCapture}`;    
+const removeLi = function(){    
+    while (listaUL.childElementCount > 0) {
+        listaUL.firstElementChild.remove();
+    }
 }
 
-let inputDate;
+function cleanArray (array) {    
+    const newArrayClean= array.splice(0,array.length);
+    return newArrayClean;    
+}
+
+//HTML. capturar dato por dato
+const capturarDatoArray = function() {    
+    let numberCapture = inputCapture.value;    
+    if(numberCapture != "") {
+        crearArray(numberCapture);        
+        let etiquetaLI = document.createElement("li");
+        listaUL.appendChild(etiquetaLI);
+        etiquetaLI.innerText =`${numberCapture}`;    
+    }
+}
+
 const comprobarEnter = (key) => {    
-    debugger
-    const teclaCode = parseInt(key.keyCode);
-        if (teclaCode === 13 ) {        
-            console.log(inputCapture.value);
-            
-            capturarDatoArray();
-        // inputDate = undefined;
-        // inputCapture.value = "";
-        }
-    // else if  (inputDate > 0) {
-    //     inputDate += key.key;
-    //     inputCapture.value = inputDate;
-    // } else {
-    //     inputDate = key.key;
-    //     inputCapture.value = inputDate;
-    // }    
-    key.stopPropagation()
-    // });
+    const teclaCode = key.charCode;
+        if (teclaCode === 13 ) {
+            key.preventDefault();
+            console.log(inputCapture.value);            
+            capturarDatoArray();        
+            inputCapture.value = "";
+        }         
+    key.stopPropagation();    
+}
+
+const reiniciarConteo = function () {
+    inputCapture.value = "";
+    cleanArray(listCaptureHTML);
+    cleanArray(resultModa);
+    listaUL.classList.remove("capturaTerminada");
+    removeLi();
+    resultP.innerHTML = "";
 }
 
 //conexion con HTML
 const calcularModa = function() {
-    moda(listCaptureHTML);
-    const resultP = document.getElementById("ResultP");
+    moda(listCaptureHTML);    
     resultP.innerHTML =`La moda es: ${resultModa[0]}. <br> Se repite ${resultModa[1]} veces.`;    
-    listaUL.style.color = "#b8a771";
+    listaUL.classList.toggle("capturaTerminada");
+    cleanArray(listCaptureHTML);
+    cleanArray(resultModa);
 }
-
-//Habilitar captura con el tecla Enter. Hay un error al final de la ejecucion
 
 buttonCapture.addEventListener("click", capturarDatoArray);
 buttonResult.addEventListener("click", calcularModa);
 inputCapture.addEventListener("keypress", comprobarEnter);
+buttonRestart.addEventListener("click", reiniciarConteo);
